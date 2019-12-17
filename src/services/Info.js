3bed692465508f4040b58a87991c4751697a2a2b
SirRichardM <richardmoeb@gmail.com>
2019-12-17 11:57:04 -0500

    all pictures print out on classics FUCK YEAH

--- src/services/Info.js
+++ src/services/Info.js
@@ -10,11 +10,11 @@ console.log("hello")
 class Info extends Component {
   constructor(props) {
     super(props);
-    
+
     this.state = {
       movie: [],
       apiData: false,
-      apiDataTwo: false, 
+      apiDataTwo: false,
       additionalPics: []
     }
 
@@ -22,33 +22,33 @@ class Info extends Component {
 
   componentDidMount() {
     let idFor = this.props.match.params.id
-    const rez =  axios.get(`https://cors-anywhere.herokuapp.com/https://api.themoviedb.org/3/movie/${idFor}?api_key=8f5a5d2d5c46bee563141af24bce82ab`)
+    const rez = axios.get(`https://cors-anywhere.herokuapp.com/https://api.themoviedb.org/3/movie/${idFor}?api_key=8f5a5d2d5c46bee563141af24bce82ab`)
       .then(details => {
         this.setState({
           movie: {
             title: details.data.original_title
           }
-      })
+        })
         axios.get(`http://api.giphy.com/v1/gifs/search?q=${this.state.movie.title}&api_key=VC8tFxRJWVzQj5LrvDTYc0YsEgUm4EhH&limit=3`)
-        .then(gif => {
-        
-          this.setState({
-            movie: {
-              title: details.data.original_title,
-              overview: details.data.overview,
-              release: details.data.release_date,
-              tagline: details.data.tagline,
-              runtime: details.data.runtime,
-              backdrop: (pic + details.data.backdrop_path),
-              budget: details.data.budget,
-              gifs: gif.data.data[0].embed_url,
-              apiData: true
+          .then(gif => {
 
-            }
+            this.setState({
+              movie: {
+                title: details.data.original_title,
+                overview: details.data.overview,
+                release: details.data.release_date,
+                tagline: details.data.tagline,
+                runtime: details.data.runtime,
+                backdrop: (pic + details.data.backdrop_path),
+                budget: details.data.budget,
+                gifs: gif.data.data[0].embed_url,
+                apiData: true
+
+              }
+            })
+
           })
 
-        })
-     
         // axios.get(`http://api.giphy.com/v1/gifs/search?q=${this.state.movie.title}&api_key=VC8tFxRJWVzQj5LrvDTYc0YsEgUm4EhH&limit=1`)
         //   .then(gif => {
         //     this.setState({
@@ -56,10 +56,10 @@ class Info extends Component {
         //         gifs: gif.data.data[0].embed_url,
         //         apiData: true
         //       }
-            
+
         //   })
-        
-          
+
+
         //   })
 
         axios.get(`https://cors-anywhere.herokuapp.com/https://api.themoviedb.org/3/movie/${idFor}/images?api_key=8f5a5d2d5c46bee563141af24bce82ab`)
@@ -68,32 +68,33 @@ class Info extends Component {
             const newPics = [];
             for (let i = 0; i < 10; i++) {
               newPics.push(image.data.backdrops[i].file_path)
-            
-              
+
+
             }
             this.setState({
-              additionalPics: [...this.state.additionalPics, newPics]
+              additionalPics: [...newPics]
             })
             console.log(this.state.additionalPics)
-        })
-      
-    })
-    
+          })
+
+      })
 
 
-    
-    
+
+
+
   }
 
 
 
-  render () {
+  render() {
     console.log(this.props.match.params.id)
     console.log(this.state.movie.apiData)
     console.log(this.state.movie.gifs)
+    console.log(this.state.additionalPics)
     return (
       <div>
-        {this.state.movie.apiData === true  ?
+        {this.state.movie.apiData === true ?
           <div className="stats">
             <h1>{this.state.movie.title}</h1>
             <h2>Released on: {this.state.movie.release}</h2>
@@ -103,11 +104,21 @@ class Info extends Component {
             <embed src={this.state.movie.gifs} />
             <p>{this.state.movie.overview}</p>
             <img src={this.state.movie.backdrop} />
-            
+
             <h2>Runtime: {this.state.movie.runtime} Mins</h2>
             <h2>Budget: $ {this.state.movie.budget}</h2>
           </div>
-          : <h1>Loading..</h1>}  
+          : <h1>Loading..</h1>}
+        <div>
+          {this.state.additionalPics.map((pics, i) =>
+            <div key={i}>
+              <img src={pic + pics} />
+              <div>
+                {pics}
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     );
   }
